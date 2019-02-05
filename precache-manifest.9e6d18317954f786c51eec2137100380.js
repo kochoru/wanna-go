20a3fb0bbe56d013a4bb259312a9f81ce4f5f571
kochoru <kochoru333@gmail.com>
2019-02-05 19:40:11 +0900

    Updates

---
+++ precache-manifest.9e6d18317954f786c51eec2137100380.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22f07c9c0a7522bb4b98",
+    "url": "/wanna-go/static/js/runtime~main.22f07c9c.js"
+  },
+  {
+    "revision": "1d024bc96d9e11ef0caf",
+    "url": "/wanna-go/static/js/main.1d024bc9.chunk.js"
+  },
+  {
+    "revision": "d7057ea3fb11fc00baa5",
+    "url": "/wanna-go/static/js/1.d7057ea3.chunk.js"
+  },
+  {
+    "revision": "1d024bc96d9e11ef0caf",
+    "url": "/wanna-go/static/css/main.19695822.chunk.css"
+  },
+  {
+    "revision": "61d2a91e4fce69bc86ab4fdc9152d10b",
+    "url": "/wanna-go/index.html"
+  }
+];
